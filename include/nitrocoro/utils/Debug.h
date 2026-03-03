77da98ab3494bde8d2a6bdc42d7f85042c3622d3
He, Wanchen <hwc14@qq.com>
2026-03-03 21:12:43 +0800

    log level.

--- include/nitrocoro/utils/Debug.h
+++ include/nitrocoro/utils/Debug.h
@@ -1,13 +1,78 @@
 #pragma once
 
 #include <cstdio>
+#include <cstring>
 
-#define NITRO_ERROR(...) printf(__VA_ARGS__)
-#define NITRO_INFO(...) printf(__VA_ARGS__)
+namespace nitrocoro
+{
+
+enum class LogLevel
+{
+    Trace = 0,
+    Debug = 1,
+    Info = 2,
+    Error = 3,
+    Off = 4
+};
+
+void setLogLevel(LogLevel level);
+LogLevel getLogLevel();
+
+// Helper to extract filename from path
+inline const char * extractFilename(const char * path)
+{
+    const char * file = strrchr(path, '/');
+    if (!file)
+        file = strrchr(path, '\\');
+    return file ? file + 1 : path;
+}
+
+} // namespace nitrocoro
+
+#define NITRO_ERROR(...)                                                            \
+    do                                                                              \
+    {                                                                               \
+        if (::nitrocoro::getLogLevel() <= ::nitrocoro::LogLevel::Error)             \
+        {                                                                           \
+            printf("[ERROR] ");                                                     \
+            printf(__VA_ARGS__);                                                    \
+            printf(" - %s:%d\n", ::nitrocoro::extractFilename(__FILE__), __LINE__); \
+        }                                                                           \
+    } while (0)
+
+#define NITRO_INFO(...)                                                             \
+    do                                                                              \
+    {                                                                               \
+        if (::nitrocoro::getLogLevel() <= ::nitrocoro::LogLevel::Info)              \
+        {                                                                           \
+            printf("[INFO] ");                                                      \
+            printf(__VA_ARGS__);                                                    \
+            printf(" - %s:%d\n", ::nitrocoro::extractFilename(__FILE__), __LINE__); \
+        }                                                                           \
+    } while (0)
 
 #ifndef NDEBUG
-#define NITRO_DEBUG(...) printf(__VA_ARGS__)
-#define NITRO_TRACE(...) printf(__VA_ARGS__)
+#define NITRO_DEBUG(...)                                                            \
+    do                                                                              \
+    {                                                                               \
+        if (::nitrocoro::getLogLevel() <= ::nitrocoro::LogLevel::Debug)             \
+        {                                                                           \
+            printf("[DEBUG] ");                                                     \
+            printf(__VA_ARGS__);                                                    \
+            printf(" - %s:%d\n", ::nitrocoro::extractFilename(__FILE__), __LINE__); \
+        }                                                                           \
+    } while (0)
+
+#define NITRO_TRACE(...)                                                            \
+    do                                                                              \
+    {                                                                               \
+        if (::nitrocoro::getLogLevel() <= ::nitrocoro::LogLevel::Trace)             \
+        {                                                                           \
+            printf("[TRACE] ");                                                     \
+            printf(__VA_ARGS__);                                                    \
+            printf(" - %s:%d\n", ::nitrocoro::extractFilename(__FILE__), __LINE__); \
+        }                                                                           \
+    } while (0)
 #else
 #define NITRO_DEBUG(...) ((void)0)
 #define NITRO_TRACE(...) ((void)0)
---
+++ src/Debug.cc
@@ -0,0 +1,41 @@
+/**
+ * @file Debug.cc
+ * @brief Debug utilities implementation
+ */
+#include <cstdlib>
+#include <cstring>
+#include <nitrocoro/utils/Debug.h>
+
+namespace nitrocoro
+{
+
+static LogLevel parseLogLevel(const char * str)
+{
+    if (!str)
+        return LogLevel::Info;
+    if (strcmp(str, "TRACE") == 0 || strcmp(str, "trace") == 0)
+        return LogLevel::Trace;
+    if (strcmp(str, "DEBUG") == 0 || strcmp(str, "debug") == 0)
+        return LogLevel::Debug;
+    if (strcmp(str, "INFO") == 0 || strcmp(str, "info") == 0)
+        return LogLevel::Info;
+    if (strcmp(str, "ERROR") == 0 || strcmp(str, "error") == 0)
+        return LogLevel::Error;
+    if (strcmp(str, "OFF") == 0 || strcmp(str, "off") == 0)
+        return LogLevel::Off;
+    return LogLevel::Info;
+}
+
+static LogLevel g_logLevel = parseLogLevel(std::getenv("NITROCORO_LOG_LEVEL"));
+
+void setLogLevel(LogLevel level)
+{
+    g_logLevel = level;
+}
+
+LogLevel getLogLevel()
+{
+    return g_logLevel;
+}
+
+} // namespace nitrocoro
